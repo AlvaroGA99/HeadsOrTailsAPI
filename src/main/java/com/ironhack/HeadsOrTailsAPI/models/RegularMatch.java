a24a143da566394f722a5e7e7456709a3e1d3c00
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-05-07 19:11:20 +0200

    Match entities and user relations

--- src/main/java/com/ironhack/HeadsOrTailsAPI/models/RegularMatch.java
+++ src/main/java/com/ironhack/HeadsOrTailsAPI/models/RegularMatch.java
@@ -1,4 +1,24 @@
 package com.ironhack.HeadsOrTailsAPI.models;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@DiscriminatorValue("0")
+@Data
 public class RegularMatch extends Match{
+
+    public RegularMatch(Long id, boolean headsWinner, int headsBet, int tailsBet, Date date, User headsUser, User tailsUser) {
+        super(id, headsWinner, headsBet, tailsBet, date, headsUser, tailsUser);
+    }
+
+    public RegularMatch(){
+
+    }
 }
