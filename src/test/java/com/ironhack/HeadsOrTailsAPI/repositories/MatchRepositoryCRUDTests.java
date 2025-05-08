9fb7ccfaccc64849a96d99687dbbbbb376982253
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-05-08 13:00:31 +0200

    User repsitory CRUD test template

--- src/test/java/com/ironhack/HeadsOrTailsAPI/repositories/MatchRepositoryCRUDTests.java
+++ src/test/java/com/ironhack/HeadsOrTailsAPI/repositories/MatchRepositoryCRUDTests.java
@@ -11,4 +11,6 @@ public class MatchRepositoryCRUDTests {
      @Autowired
      UserRepository userRepository;
 
+
+
 }
--- src/test/java/com/ironhack/HeadsOrTailsAPI/repositories/UserRepositoryCRUDTests.java
+++ src/test/java/com/ironhack/HeadsOrTailsAPI/repositories/UserRepositoryCRUDTests.java
@@ -1,7 +1,19 @@
 package com.ironhack.HeadsOrTailsAPI.repositories;
 
+import com.ironhack.HeadsOrTailsAPI.models.ERole;
+import com.ironhack.HeadsOrTailsAPI.models.Role;
+import com.ironhack.HeadsOrTailsAPI.models.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserRepositoryCRUDTests {
@@ -11,4 +23,82 @@ public class UserRepositoryCRUDTests {
 
     @Autowired
     UserRepository userRepository;
+
+    User user;
+    Role role;
+
+    @BeforeEach
+    void setUp() {
+        role = new Role();
+        role.setName(ERole.ROLE_USER);
+        roleRepository.save(role);
+
+        user = new User();
+        user.setUsername("testUser");
+        user.setPassword("testPassword");
+        user.setCoins(100);
+        user.setRoles(Set.of(role));
+        user.setElo(1200);
+
+
+        userRepository.save(user);
+    }
+
+    @AfterEach
+    void tearDown() {
+        roleRepository.delete(role);
+        userRepository.delete(user);
+    }
+
+
+    @Test
+    void testSaveUser() {
+
+    }
+
+    @Test
+    void testFindAll(){
+
+    }
+
+    @Test
+    void testFindByID(){
+
+
+    }
+
+    @Test
+    void testFindByUsername(){
+
+    }
+
+    @Test
+    void testFindByElo(){
+
+    }
+
+    @Test
+    void testFindByCoins(){
+
+    }
+
+    @Test
+    void testFindByRole(){
+
+    }
+
+    @Test
+    void testDeleteById(){
+
+    }
+
+    @Test
+    void testDeleteAll(){
+
+    }
+
+    @Test
+    void testUpdate(){
+
+    }
 }
