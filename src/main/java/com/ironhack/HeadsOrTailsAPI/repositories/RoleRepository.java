2c9be8fda28aa291b2864eceb7c2b74b30921647
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-05-08 12:25:20 +0200

    First TDD repositories iteration

--- src/main/java/com/ironhack/HeadsOrTailsAPI/repositories/RoleRepository.java
+++ src/main/java/com/ironhack/HeadsOrTailsAPI/repositories/RoleRepository.java
@@ -3,5 +3,5 @@ package com.ironhack.HeadsOrTailsAPI.repositories;
 import com.ironhack.HeadsOrTailsAPI.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepoository extends JpaRepository<Role,Long> {
+public interface RoleRepository extends JpaRepository<Role,Long> {
 }
